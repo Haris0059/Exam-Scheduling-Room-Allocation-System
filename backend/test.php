9a08de60f54289a2085670c790d379b15d94fcb3
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-10-22 16:27:25 +0200

    Created test.php

---
+++ backend/test.php
@@ -0,0 +1,11 @@
+<?php
+
+$student_dao = new StudentDaov4();
+$students = $student_dao->get_all();
+print_r($students);
+$student = $student_dao->get_by_id(1);
+print_r($student);
+$student_dao->add(["name" => "James", "email" => "james@gmail.com"]);
+$student_dao->update(["name" => "James", "email" => "jamesupdate@gmail.com"], 3);
+$student_dao->delete(3);
+print_r($student_dao->get_all());
