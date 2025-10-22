2dd995bcaa90376e3d785116c3665cc417a00a20
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-10-22 16:26:45 +0200

    Created StudentDao.php

---
+++ backend/dao/StudentDao.php
@@ -0,0 +1,57 @@
+<?php
+require_once __DIR__ . '/BaseDao.php';
+
+class StudentDao extends BaseDao
+{
+    protected $table_name;
+
+    public function __construct()
+    {
+        $this->table_name = "students";
+        parent::__construct($this->table_name);
+    }
+
+    public function get_all()
+    {
+        return $this->query('SELECT * FROM ' . $this->table_name, []);
+    }
+
+    public function get_by_id($id)
+    {
+        return $this->query_unique('SELECT * FROM ' . $this->table_name . ' WHERE id=:id', ['id' => $id]);
+    }
+
+    public function get_by_email($email)
+    {
+        return $this->query_unique('SELECT * FROM ' . $this->table_name . ' WHERE email=:email', ['email' => $email]);
+    }
+
+    public function get_by_department($department_id)
+    {
+        return $this->query_unique('SELECT * FROM ' . $this->table_name . ' WHERE department_id=:department_id', ['department_id' => $department_id]);
+    }
+
+    public function count_students_paginated($search)
+    {
+        $query = "SELECT COUNT(*) AS count
+                  FROM " . $this->table_name . "
+                  WHERE LOWER(name) LIKE CONCAT('%', :search, '%') OR 
+                        LOWER(email) LIKE CONCAT('%', :search, '%');";
+        return $this->query_unique($query, [
+            'search' => $search
+        ]);
+    }
+
+    public function get_students_paginated($offset, $limit, $search, $order_column, $order_direction)
+    {
+        $query = "SELECT *
+                  FROM " . $this->table_name . "
+                  WHERE LOWER(name) LIKE CONCAT('%', :search, '%') OR 
+                        LOWER(email) LIKE CONCAT('%', :search, '%')
+                  ORDER BY {$order_column} {$order_direction}
+                  LIMIT {$offset}, {$limit}";
+        return $this->query($query, [
+            'search' => $search
+        ]);
+    }
+}
