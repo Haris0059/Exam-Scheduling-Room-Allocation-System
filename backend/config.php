9a59a1b2d795819befae90a225e02430fd25c029
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-10-22 08:59:46 +0200

    Created config.php, including Database variables for connecting with Database

---
+++ backend/config.php
@@ -0,0 +1,33 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
+
+class Config {
+    public static function DB_NAME() {
+        return Config::get_env("DB_NAME", "esras");
+    }
+
+    public static function DB_PORT() {
+        return Config::get_env("DB_PORT", 3306);
+    }
+
+    public static function DB_USER() {
+        return Config::get_env("DB_USER", 'root');
+    }
+
+    public static function DB_PASSWORD() {
+        return Config::get_env("DB_PASSWORD", '');
+    }
+
+    public static function DB_HOST() {
+        return Config::get_env("DB_HOST", '127.0.0.1');
+    }
+
+    public static function get_env($name, $default) {
+        if (isset($_ENV[$name]) && trim($_ENV[$name]) != "") {
+            return $_ENV[$name];
+        } return $default;
+    }
+}
