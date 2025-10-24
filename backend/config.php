3e1afde8e28726d0f5f1389dd2e8ab62f6c81bab
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-10-24 10:46:54 +0200

    Changed config.php

--- backend/config.php
+++ backend/config.php
@@ -1,33 +1,35 @@
 <?php
 
+// Set the reporting
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
 
-class Config {
-    public static function DB_NAME() {
-        return Config::get_env("DB_NAME", "esras");
+class Config
+{
+    public static function DB_NAME()
+    {
+        return 'esras'; 
+    }
+    public static function DB_PORT()
+    {
+        return  3306;
+    }
+    public static function DB_USER()
+    {
+        return 'root';
+    }
+    public static function DB_PASSWORD()
+    {
+        return '';
+    }
+    public static function DB_HOST()
+    {
+        return '127.0.0.1';
     }
 
-    public static function DB_PORT() {
-        return Config::get_env("DB_PORT", 3306);
-    }
-
-    public static function DB_USER() {
-        return Config::get_env("DB_USER", 'root');
-    }
-
-    public static function DB_PASSWORD() {
-        return Config::get_env("DB_PASSWORD", '');
-    }
-
-    public static function DB_HOST() {
-        return Config::get_env("DB_HOST", '127.0.0.1');
-    }
-
-    public static function get_env($name, $default) {
-        if (isset($_ENV[$name]) && trim($_ENV[$name]) != "") {
-            return $_ENV[$name];
-        } return $default;
+    public static function JWT_SECRET() 
+    {
+        return 'example_string';
     }
 }
