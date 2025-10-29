1ce0bca523a308b0327143702931c7e81e0adf2c
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-10-29 15:11:37 +0100

    fixes

---
+++ backend/dao/RoomDao.php
@@ -0,0 +1,57 @@
+<?php
+require_once __DIR__ . '/BaseDao.php';
+
+class RoomDao extends BaseDao
+{
+    protected $table_name;
+
+    public function __construct()
+    {
+        $this->table_name = "rooms";
+        parent::__construct($this->table_name);
+    }
+
+    public function get_all()
+    {
+        return $this->query('SELECT * FROM ' . $this->table_name, []);
+    }
+
+    public function get_by_id($id)
+    {
+        return $this->query_unique('SELECT * FROM ' . $this->table_name . ' WHERE id=:id', ['id' => $id]);
+    }
+
+    public function get_by_code($code)
+    {
+        return $this->query_unique('SELECT * FROM ' . $this->table_name . ' WHERE code=:code', ['code' => $code]);
+    }
+
+    public function get_by_type($type)
+    {
+        return $this->query_unique('SELECT * FROM ' . $this->table_name . ' WHERE type=:type', ['type' => $type]);
+    }
+
+    public function count_rooms_paginated($search)
+    {
+        $query = "SELECT COUNT(*) AS count
+                  FROM " . $this->table_name . "
+                  WHERE LOWER(name) LIKE CONCAT('%', :search, '%') OR 
+                        LOWER(email) LIKE CONCAT('%', :search, '%');";
+        return $this->query_unique($query, [
+            'search' => $search
+        ]);
+    }
+
+    public function get_rooms_paginated($offset, $limit, $search, $order_column, $order_direction)
+    {
+        $query = "SELECT *
+                  FROM " . $this->table_name . "
+                  WHERE LOWER(name) LIKE CONCAT('%', :search, '%') OR 
+                        LOWER(email) LIKE CONCAT('%', :search, '%')
+                  ORDER BY {$order_column} {$order_direction}
+                  LIMIT {$offset}, {$limit}";
+        return $this->query($query, [
+            'search' => $search
+        ]);
+    }
+}
