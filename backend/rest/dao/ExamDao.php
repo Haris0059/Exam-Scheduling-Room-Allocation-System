01bd7a3b22ad74578f5e4c8568aeba98c469269d
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-10-31 17:23:28 +0100

    Removed Pagination from Exams

--- backend/rest/dao/ExamDao.php
+++ backend/rest/dao/ExamDao.php
@@ -30,28 +30,4 @@ class ExamDao extends BaseDao
     {
         return $this->query_unique('SELECT * FROM ' . $this->table_name . ' WHERE type=:type', ['type' => $type]);
     }
-
-    public function count_exams_paginated($search)
-    {
-        $query = "SELECT COUNT(*) AS count
-                  FROM " . $this->table_name . "
-                  WHERE LOWER(name) LIKE CONCAT('%', :search, '%') OR 
-                        LOWER(email) LIKE CONCAT('%', :search, '%');";
-        return $this->query_unique($query, [
-            'search' => $search
-        ]);
-    }
-
-    public function get_exams_paginated($offset, $limit, $search, $order_column, $order_direction)
-    {
-        $query = "SELECT *
-                  FROM " . $this->table_name . "
-                  WHERE LOWER(name) LIKE CONCAT('%', :search, '%') OR 
-                        LOWER(email) LIKE CONCAT('%', :search, '%')
-                  ORDER BY {$order_column} {$order_direction}
-                  LIMIT {$offset}, {$limit}";
-        return $this->query($query, [
-            'search' => $search
-        ]);
-    }
 }
