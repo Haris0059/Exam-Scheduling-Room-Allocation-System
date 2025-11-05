78dc1e8d8f0e44d2b0645430c4619f9e4faad2b4
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-11-05 16:42:12 +0100

    Implemented for RoomService.php functions for: - validationCheck which is generic validation check for each input field such as code,type,seat capacity or coordinates. - addRoom and updateRoom which uses validationCheck - removeRoom which checks if Room ID is provided and if the Room with that ID exists

---
+++ backend/rest/services/RoomService.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once __DIR__ . '/BaseService.php';
+require_once __DIR__ . '/../dao/RoomDao.php';
+
+class RoomService extends BaseService
+{
+    protected $roomDao;
+
+    public function __construct()
+    {
+        $dao = new RoomDao();
+
+        parent::__construct($dao);
+    }
+
+    public function addRoom($data)
+    {
+        $this->validationCheck($data);
+
+        return $this->dao->insert($data);
+    }
+
+    public function updateRoom($id, $data)
+    {
+        $this->validationCheck($data);
+        
+        return $this->dao->update($id, $data);
+    }
+
+    public function validationCheck($data) {
+        if (empty($data['code'])) {
+            throw new Exception("Room code is required.");
+        } else if (empty($data['type'])) {
+            throw new Exception("Room type is required.");
+        } else if (empty($data['seat_capacity'])) {
+            throw new Exception("Seat capacity is required.");
+        } else if (empty($data['coordinates'])) {
+            throw new Exception("Coordinates are required.");
+        }
+    }
+
+    public function removeRoom($id)
+    {
+        if (empty($id) || !is_numeric($id) || $id <= 0) {
+            throw new Exception("Invalid Room ID provided.");
+        } else if (!$this->dao->getById($id)) {
+            throw new Exception("Room with the given ID does not exist.");
+        }
+
+        return $this->dao->delete($id);
+    }
+
+}
