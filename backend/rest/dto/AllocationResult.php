5cef66f2a7cac3a311699a3d104e88f86c28cd99
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-11-11 22:39:16 +0100

    DTO implementation

---
+++ backend/rest/dto/AllocationResult.php
@@ -0,0 +1,11 @@
+<?php
+class AllocationResult {
+    /** @var ParsedRoom[] */
+    public $rooms = [];
+    public $cost = 999999; // high default "cost" (e.g., max distance)
+    public $multi_floor = false;
+    public $total_capacity = 0;
+    
+    // success/failure status
+    public $success = false; 
+}
---
+++ backend/rest/dto/ParsedRoom.php
@@ -0,0 +1,17 @@
+<?php
+class ParsedRoom {
+    public $id;
+    public $capacity;
+    public $x;
+    public $y;
+    public $z; // This is the floor number
+    public $distance_from_anchor = 0;
+
+    public function __construct($db_row) {
+        $this->id = (int)$db_row['room_id'];
+        $this->capacity = (int)$db_row['seat_capacity'];
+        $this->x = (int)$db_row['coord_x'];
+        $this->y = (int)$db_row['coord_y'];
+        $this->z = (int)$db_row['floor'];
+    }
+}
