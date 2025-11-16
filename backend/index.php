77cba26b2c1303df6059bc1b12c9d3e58ff218d9
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-11-16 12:02:18 +0100

    fixes

--- backend/index.php
+++ backend/index.php
@@ -2,17 +2,18 @@
 require 'vendor/autoload.php'; //run autoloader
 
 // All services
-require_once 'rest/services/BaseService.php';
-require_once 'rest/services/CourseService.php';
-require_once 'rest/services/EmployeeService.php';
-require_once 'rest/services/ExamService.php';
-require_once 'rest/services/RoomAllocationService.php';
-require_once 'rest/services/RoomService.php';
-require_once 'rest/services/StudentService.php';
+require_once __DIR__ . '/rest/services/BaseService.php';
+require_once __DIR__ . '/rest/services/CourseService.php';
+require_once __DIR__ . '/rest/services/EmployeeService.php';
+require_once __DIR__ . '/rest/services/ExamService.php';
+require_once __DIR__ . '/rest/services/RoomAllocationService.php';
+require_once __DIR__ . '/rest/services/RoomService.php';
+require_once __DIR__ . '/rest/services/StudentService.php';
+
 
 // All routes
 //require_once 'rest/routes/BaseRoute.php';
-require_once 'rest/routes/CourseRoute.php';
+require_once '/rest/routes/CourseRoute.php';
 //require_once 'rest/routes/EmployeeRoute.php';
 //require_once 'rest/routes/ExamRoute.php';
 //require_once 'rest/routes/RoomAllocationRoute.php';
