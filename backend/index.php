940712697c8007a2d4a2b24c83ac17f35b12ff85
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-11-16 12:04:35 +0100

    test

--- backend/index.php
+++ backend/index.php
@@ -1,24 +1,6 @@
 <?php
 require 'vendor/autoload.php'; //run autoloader
 
-// All services
-require_once __DIR__ . '/rest/services/BaseService.php';
-require_once __DIR__ . '/rest/services/CourseService.php';
-require_once __DIR__ . '/rest/services/EmployeeService.php';
-require_once __DIR__ . '/rest/services/ExamService.php';
-require_once __DIR__ . '/rest/services/RoomAllocationService.php';
-require_once __DIR__ . '/rest/services/RoomService.php';
-require_once __DIR__ . '/rest/services/StudentService.php';
-
-
-// All routes
-//require_once 'rest/routes/BaseRoute.php';
-require_once '/rest/routes/CourseRoute.php';
-//require_once 'rest/routes/EmployeeRoute.php';
-//require_once 'rest/routes/ExamRoute.php';
-//require_once 'rest/routes/RoomAllocationRoute.php';
-//require_once 'rest/routes/RoomRoute.php';
-//require_once 'rest/routes/StudentRoute.php';
 
 // Default route
 Flight::route('/', function(){  
