723142d0ded58cf0935820fb5621453dc23ccb87
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-12-11 23:04:41 +0100

    Deleted files

--- frontend/js/demo/datatables-demo.js
+++
@@ -1,8 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTableRooms').DataTable();
-});
-
-$(document).ready(function() {
-  $('#dataTableEmployees').DataTable();
-});
