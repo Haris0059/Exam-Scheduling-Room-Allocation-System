2cd8a46e1f8ff6b6262e2459b5d6c17edf41e549
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-12-11 23:05:05 +0100

    Updated index.php for JWT and Auth

--- backend/index.php
+++ backend/index.php
@@ -4,7 +4,17 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+require_once __DIR__ . "/middleware/AuthMiddleware.php";
+Flight::register('auth_middleware', "AuthMiddleware");
+
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 require_once __DIR__ . '/rest/services/BaseService.php';
+
+require_once __DIR__ . '/rest/services/AuthService.php';
+Flight::register('auth_service', "AuthService");
 
 require_once __DIR__ . '/rest/services/EmployeeService.php';
 Flight::register('employee_service', 'EmployeeService');
@@ -27,23 +37,48 @@ Flight::register('room_allocation_service', 'RoomAllocationService');
 require_once __DIR__ . '/rest/services/ExamService.php';
 Flight::register('exam_service', 'ExamService');
 
+Flight::route('/*', function() {
 
-require_once __DIR__ . '/rest/routes/employee_route.php';
-require_once __DIR__ . '/rest/routes/student_route.php';
-require_once __DIR__ . '/rest/routes/course_route.php';
-require_once __DIR__ . '/rest/routes/course_enrollment_route.php';
-require_once __DIR__ . '/rest/routes/room_route.php';
-require_once __DIR__ . '/rest/routes/room_allocation_route.php';
-require_once __DIR__ . '/rest/routes/exam_route.php';
+    $url = Flight::request()->url;
+
+    // Allow home page and login without authentication
+    if ($url === '/' || strpos($url, '/auth/login') === 0) {
+        return TRUE;
+    }
+
+    try {
+        $token = Flight::request()->getHeader("Authentication");
+
+        if (!$token) {
+            Flight::halt(401, "Missing authentication header");
+        }
+
+        $decoded_token = JWT::decode($token, new Key(Config::JWT_SECRET(), 'HS256'));
+
+        Flight::set('user', $decoded_token->user);
+        Flight::set('jwt_token', $token);
+
+        return TRUE;
+
+    } catch (\Exception $e) {
+        Flight::halt(401, $e->getMessage());
+    }
+});
 
 
-
+require_once __DIR__ . '/rest/routes/EmployeeRoute.php';
+require_once __DIR__ . '/rest/routes/StudentRoute.php';
+require_once __DIR__ . '/rest/routes/CourseRoute.php';
+require_once __DIR__ . '/rest/routes/CourseEnrollmentRoute.php';
+require_once __DIR__ . '/rest/routes/RoomRoute.php';
+require_once __DIR__ . '/rest/routes/RoomAllocationRoute.php';
+require_once __DIR__ . '/rest/routes/ExamRoute.php';
+require_once __DIR__ . '/rest/routes/AuthRoute.php';
 
 // Default route
 Flight::route('/', function(){  
    echo 'Exam Scheduling and Room Allocation System API is running!';
 });
-
 
 Flight::start();  //start FlightPHP
 ?>
