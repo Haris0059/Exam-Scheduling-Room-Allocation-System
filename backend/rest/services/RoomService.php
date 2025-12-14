5c06c032adbfe9554e0f6ac8078666f1b51221b5
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-12-14 14:44:50 +0100

    Implemented a new type of validation for RoomService.php

--- backend/rest/services/RoomService.php
+++ backend/rest/services/RoomService.php
@@ -14,17 +14,66 @@ class RoomService extends BaseService
         parent::__construct($dao);
     }
 
+    private function validateRoomData($data)
+    {
+        // code
+        if (!isset($data['code']) || !is_numeric($data['code'])) {
+            throw new Exception("Room code is required and must be a number.");
+        }
+        if ((int)$data['code'] < 0 || strlen((string)$data['code']) > 3) {
+            throw new Exception("Room code must be a numeric value with max 3 digits.");
+        }
+
+        // type
+        if (isset($data['type']) && !in_array($data['type'], ['standard', 'it', 'lecturehall'])) {
+            throw new Exception("Invalid room type. Allowed values: standard, it, lecturehall.");
+        }
+
+        // seat_capacity
+        if (!isset($data['seat_capacity']) || !is_numeric($data['seat_capacity'])) {
+            throw new Exception("Seat capacity is required and must be a number.");
+        }
+        if ((int)$data['seat_capacity'] <= 0) {
+            throw new Exception("Seat capacity must be greater than 0.");
+        }
+
+        // coordinates
+        if (!isset($data['coord_x']) || !is_numeric($data['coord_x'])) {
+            throw new Exception("Coordinate X must be a valid number.");
+        }
+        if (!isset($data['coord_y']) || !is_numeric($data['coord_y'])) {
+            throw new Exception("Coordinate Y must be a valid number.");
+        }
+        if (!isset($data['coord_z']) || !is_numeric($data['coord_z'])) {
+            throw new Exception("Coordinate Z must be a valid number.");
+        }
+    }
+
+    private function validateRoomUpdate($id, $data)
+    {
+        // id
+        if (empty($id) || !is_numeric($id) || $id <= 0) {
+            throw new Exception("Invalid Room ID.");
+        }
+
+        if (!$this->dao->getById($id)) {
+            throw new Exception("Room with the given ID does not exist.");
+        }
+
+        // reuse same rules
+        $this->validateRoomData($data);
+    }
+
+
     public function addRoom($data)
     {
-        $this->validationCheck($data);
-
+        $this->validateRoomData($data);
         return $this->dao->add($data);
     }
 
     public function updateRoom($id, $data)
     {
-        //$this->validationCheck($data);
-        
+        $this->validateRoomUpdate($id, $data);
         return $this->dao->update($data, $id);
     }
 
@@ -37,18 +86,6 @@ class RoomService extends BaseService
         }
 
         return $this->dao->delete($id);
-    }
-
-    public function validationCheck($data) {
-        if (empty($data['code'])) {
-            throw new Exception("Room code is required.");
-        } else if (empty($data['type'])) {
-            throw new Exception("Room type is required.");
-        } else if (empty($data['seat_capacity'])) {
-            throw new Exception("Seat capacity is required.");
-        } else if (empty($data['coord_x']) || empty($data['coord_y']) || empty($data['coord_z'])) {
-            throw new Exception("Coordinates are required.");
-        }
     }
 
     public function getRoomsPaginated($offset, $limit, $search, $order_column, $order_direction)
@@ -64,16 +101,28 @@ class RoomService extends BaseService
 
     public function getByCode($code)
     {
+        if (!is_numeric($code)) {
+            throw new Exception("Room code must be numeric.");
+        }
+
         return $this->dao->getByCode($code);
     }
 
     public function getByType($type)
     {
+        if (!in_array($type, ['standard', 'it', 'lecturehall'])) {
+            throw new Exception("Invalid room type.");
+        }
+
         return $this->dao->getByType($type);
     }
     
     public function getBySeatCapacity($seat_capacity)
     {
+        if (!is_numeric($seat_capacity) || $seat_capacity <= 0) {
+            throw new Exception("Seat capacity must be a positive number.");
+        }
+
         return $this->dao->getBySeatCapacity($seat_capacity);
     }
 }
